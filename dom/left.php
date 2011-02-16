8d902eb7d9738cbad7c3f47a65ce25ee0e4a85cf
edofic <draljo@gmail.com>
2011-02-16 21:57:35 +0100

    avtomatsko pridobivanje imena datoteke

--- dom/left.php
+++ dom/left.php
@@ -1,16 +1,29 @@
 <?php
-$url="left.htm";
-
-
-# create and load the HTML
-include('simple_html_dom.php');
-$html = new simple_html_dom();
-$html->load_file($url);
-
-$links = $html->find("a");
-
-foreach($links as $link)
+function getFilename()
 {
-echo $link->innertext . "<br>";
+	$url="left.htm"; ##TODO set the real one
+
+	# create and load the HTML
+	include('simple_html_dom.php');
+	$html = new simple_html_dom();
+	$html->load_file($url);
+
+	$links = $html->find("a");
+
+	$now = time();##mktime(15,0,1,2,3); #for testing
+	$rightChoice="";
+
+	foreach($links as $link)
+	{
+		$time=strtotime($link->plaintext);
+		if(($time+16*3600)>=$now) #show today's file until 16.00
+			$rightChoice=$link->href;
+		else
+			break;
+	}
+	return $rightChoice;
 }
+
+echo getFilename();
+
 ?>
